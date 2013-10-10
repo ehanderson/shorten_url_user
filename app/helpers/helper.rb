9bed38501f1b555d95123d4fb5776c486f7d143c
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 07:58:02 -0500

    need to make private page

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,19 +1,19 @@
-helpers do
-  # This will return the current user, if they exist
-  # Replace with code that works with your application
-  def current_user
-    if session[:authetic]
-      @current_user ||= User.find_by_id(session[:authetic])
-    end
-  end
+# helpers do
+#   # This will return the current user, if they exist
+#   # Replace with code that works with your application
+#   def current_user
+#     if session[:authetic]
+#       @current_user ||= User.find_by_id(session[:authetic])
+#     end
+#   end
 
-  # Returns true if current_user exists, false otherwise
-  def logged_in?
-    !current_user.nil?
-  end
-end
-#############THIS IS WHERE SHIT WONT CONTINUE###########
-@user = User.find_by_id(current_user.id)
-@url = Url.create(...)
-@user.urls << @url
+#   # Returns true if current_user exists, false otherwise
+#   def logged_in?
+#     !current_user.nil?
+#   end
+# end
+# #############THIS IS WHERE SHIT WONT CONTINUE###########
+# @user = User.find_by_id(current_user.id)
+# @url = Url.create(...)
+# @user.urls << @url
 
